9dc9314623b759813efaa948e288aee92890e0c8
“Konstantinos <kostaks@outlook.com>
2023-02-14 20:17:30 +0100

    done

--- public/js/script.js
+++ public/js/script.js
@@ -76,32 +76,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
   })
 
-
-//change font style of heading
-document.getElementsByTagName('h4')[0].style.fontFamily = "Impact,Charcoal,sans-serif"
-
-//get the number of recipe boxes and replace the p id="total-num-recipes" inner text with this value
-let cardBodyDivs = document.getElementsByClassName('card-body') 
-document.getElementById('total-num-recipes').textContent = "You have  " + cardBodyDivs.length + " recipes so far"
-
-document.getElementById('cocktail-name').style.textTransform = "uppercase";
-//4 get all li tag and lowercase text inside
-/*
-let LITags = document.querySelectorAll('li')
-for (let i = 0; i < LITags.length; i++) {
-  LITags[i].textContent = LITags[i].textContent.toLowerCase()
-}  
-*/
-//change heading style
-let author = 'nikki'
-document.getElementById('small-display').textContent = "Welcome " + author + "! Are you looking for a special recipe today?"
-
-//change font style of heading
-document.getElementsByTagName('h1')[0].style.fontFamily = "Impact,Charcoal,sans-serif"
-
-//get the number of recipe boxes and replace the p id="total-num-recipes" inner text with this value
-//let cardBodyDivs = document.getElementsByClassName('card-body') 
-
-
 });
 
